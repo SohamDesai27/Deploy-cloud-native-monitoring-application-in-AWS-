9e42bbc0dba21ab4fb0ff3d121179bb74c28c7f5
Soham Desai <100659435+SohamDesai27@users.noreply.github.com>
2023-07-29 10:23:05 -0700

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -1,9 +1,17 @@
+# Import the Boto3 library
 import boto3
 
+# Create an ECR client object
 ecr_client = boto3.client('ecr')
 
+# Define the name of the ECR repository
 repository_name = "my_monitoring_app_image"
+
+# Create a new ECR repository using the specified repository name
 response = ecr_client.create_repository(repositoryName=repository_name)
 
-repository_uri = response ['repository']['repositoryUri']
+# Extract the repository URI from the response
+repository_uri = response['repository']['repositoryUri']
+
+# Print the repository URI to the console
 print(repository_uri)
